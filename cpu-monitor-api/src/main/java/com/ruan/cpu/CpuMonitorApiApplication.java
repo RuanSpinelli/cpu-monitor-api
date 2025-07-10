26d4dfb83ec35064ac4172fc76b1bce85dc0d037
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-07-10 00:47:33 -0300

    project default
    I've just unzipped the .zip file that I've made from https://start.spring.io

---
+++ cpu-monitor-api/src/main/java/com/ruan/cpu/CpuMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ruan.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuMonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuMonitorApiApplication.class, args);
+	}
+
+}
---
+++ cpu-monitor-api/src/test/java/com/ruan/cpu/CpuMonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruan.cpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuMonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
